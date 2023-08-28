757ec520560e34acda6a7ffefcc0d615f9285385
Andy Ogaga <44970013+andyogaga@users.noreply.github.com>
2023-08-28 09:42:43 +0100

    fix: makes svg icon use inline color instead of class color within it. (#2)

---
+++ dist/index.js
@@ -0,0 +1,22 @@
+import { createEIP1193Provider } from "@web3-onboard/common";
+function lukso() {
+    return {
+        injectedNamespace: "lukso",
+        label: "Universal Profiles",
+        getIcon: async () => (await import("./icon.js")).default,
+        checkProviderIdentity: ({ provider }) => !!provider && !!provider.isUniversalProfileExtension,
+        getInterface: async () => {
+            if ("lukso" in window) {
+                const anyWindow = window;
+                return {
+                    provider: createEIP1193Provider(anyWindow.lukso),
+                };
+            }
+            window.open("https://chrome.google.com/webstore/detail/universal-profiles/abpickdkkbnbcoepogfhkhennhfhehfn?hl=en", "_blank");
+            throw new Error("Please install LUKSO Universal Profile extension before proceeding");
+        },
+        platforms: ["all"],
+        externalUrl: "https://chrome.google.com/webstore/detail/universal-profiles/abpickdkkbnbcoepogfhkhennhfhehfn?hl=en",
+    };
+}
+export default lukso;
